ba003a595c7ea27b5e55f9c9f8696aa7d2289a64
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-03-10 16:40:07 +1100

    Join section

--- app/components/homepage/Join.jsx
+++ app/components/homepage/Join.jsx
@@ -2,19 +2,31 @@
 
 export default function Join() {
     return (
-        <div className="container-fluid py-3">
+        <div className="container-fluid p-5">
             <div className="row">
-                <div className="col">
+                <div className="col-lg-6">
                     <img src="https://gratisography.com/wp-content/uploads/2018/05/gratisography-434H-free-stock-photo-1170x780.jpg" class="img-thumbnail img-fluid" alt="Join us image" />
                 </div>
-                <div className="col">
+                <div className="col-lg-6">
+                    <h2 className="section-title">Join Sushi Sushi today</h2>
+                    <p className="lead">Join our mailing list! We promise we won’t fill your inbox with fluffy white rice. We’re all about quality over quantity.</p>
                     <form>
-                      <div className="mb-3">
-                        <label for="inputEmail" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" />
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                      </div>
-                      <button type="submit" className="btn btn-primary">Join Us</button>
+                        <div className="row my-3">
+                            <div className="col-lg-6">
+                                <label for="inputFirstName" className="form-label">First name</label>
+                                <input type="text" className="form-control" id="inputFirstName" />
+                            </div>
+                            <div className="col-lg-6">
+                                <label for="inputLastName" className="form-label">First name</label>
+                                <input type="text" className="form-control" id="inputLastName" />
+                            </div>
+                        </div>
+                        <div className="my-3">
+                            <label for="inputEmail" className="form-label">Email address</label>
+                            <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" />
+                            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Join Us</button>
                     </form>
                 </div>
             </div>
