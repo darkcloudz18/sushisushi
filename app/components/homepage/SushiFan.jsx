f7c168d8d42c0d727882c45478fad79e3bfa5699
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-03-14 14:23:38 +1100

    sushi fan poc

---
+++ app/components/homepage/SushiFan.jsx
@@ -0,0 +1,14 @@
+'use client';
+
+export default function SushiFan() {
+    return (
+        <div class="swatch">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import './styles/homepage.scss';
 import Hero from './components/homepage/Hero'
 import Faves from './components/homepage/Faves'
 import Join from './components/homepage/Join'
+import SushiFan from './components/homepage/SushiFan'
 
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Faves />
       <Join />
+      <SushiFan />
     </>
   );
 }
