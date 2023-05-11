88ae8b8a4f89659973414510a39a196ead548fdf
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-05-11 16:58:14 +1000

    koi pond on the 404

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,38 @@
+import '../app/styles/globals.scss'
 import '../app/styles/404.scss';
 
 export default function Custom404() {
     return(
-        <div className={"not-found-main"}>
-            <h1>404 - Page Not Found</h1>
+        <div className={"container py-5"}>
+            <div className={"row"}>
+                <div className={"col-12 col-lg-6"}>
+                    <h1>Oh dear it seems you&apos;ve got lost</h1>
+                    <blockquote className="blockquote">
+                        <p>
+                            猿も木から落ちる<br/>
+                            Even monkeys fall from trees
+                        </p>
+                    </blockquote>
+                    <figcaption className="blockquote-footer">
+                        Japanese Proverb
+                    </figcaption>
+                    <p>Even the most experienced navigators get lost. Even the best web developers create broken links.</p>
+                    <p>If you think there&apos;s been an error you can email <a href={"mailto:customerfeedback@sushisushi.com.au"}>customerfeedback@sushisushi.com.au</a></p>
+                    <p>Otherwise spend some time at our koi pond and relax.</p>
+                    <a href={"/"} className={"home-link"}>Return home</a>
+                </div>
+                <div className={"col-12 col-lg-6"}>
+                    <div className={"koi-pond"}>
+                        <div className={"koi-one"}></div>
+                        <div className={"koi-two"}></div>
+                        <div className={"koi-three"}></div>
+                        <div className={"koi-four"}></div>
+                        <div className={"koi-five"}></div>
+                        <div className={"lilypad"}></div>
+                        <div className={"lotus"}></div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
