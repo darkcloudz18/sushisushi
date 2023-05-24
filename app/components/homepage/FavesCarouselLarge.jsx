72fa7076c6bd367a14a1001ff44a49fbfb317bb2
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-05-24 12:04:27 +1000

    ID namespace collision for the two faves carousels

--- app/components/homepage/FavesCarouselLarge.jsx
+++ app/components/homepage/FavesCarouselLarge.jsx
@@ -9,13 +9,13 @@ export default function FavesCarouselLarge({products}){
     },[]);
 
   return(
-    <div id="favesCarousel" className="carousel slide pt-4">
+    <div id="favesLargeCarousel" className="carousel slide pt-4">
       <div className="carousel-controls">
-        <button className="carousel-faves-prev" type="button" data-bs-target="#favesCarousel" data-bs-slide="prev">
+        <button className="carousel-faves-prev" type="button" data-bs-target="#favesLargeCarousel" data-bs-slide="prev">
           <span className="chevron left" aria-hidden="true"></span>
           <span className="visually-hidden">Previous</span>
         </button>
-        <button className="carousel-faves-next" type="button" data-bs-target="#favesCarousel" data-bs-slide="next">
+        <button className="carousel-faves-next" type="button" data-bs-target="#favesLargeCarousel" data-bs-slide="next">
           <span className="chevron right" aria-hidden="true"></span>
           <span className="visually-hidden">Next</span>
         </button>
