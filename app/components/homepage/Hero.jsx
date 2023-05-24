6d88754aad2fa3eefa0276dde2a2531e8b42f874
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-05-24 11:55:33 +1000

    better version of stand in hero img

--- app/components/homepage/Hero.jsx
+++ app/components/homepage/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
                     <div className="col-lg-6 my-3 my-lg-5 py-lg-2">
                         <h1 className="hero-title">Fresh Tuna Range</h1>
                         <p className="subtitle my-3">Your choice of Hand Roll, Sashimi or Nigiri</p>
-                        <button type="button" className="btn btn-light">Find out more</button>
+                        <a href="https://sushisushi.orderup.com.au/" className="btn btn-light">Start my order</a>
                     </div>
                 </div>
             </div>
