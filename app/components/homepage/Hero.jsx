896612afd82deab5bce11a7d70aa006261c71f76
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-05-24 13:04:12 +1000

    Little extra padding for hero

--- app/components/homepage/Hero.jsx
+++ app/components/homepage/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
         <div className="hero-container">
             <div className="container">
                 <div className="row">
-                    <div className="col-lg-6 my-3 my-lg-5 py-lg-2">
+                    <div className="col-lg-6 my-5 my-lg-5 py-lg-2">
                         <h1 className="hero-title">Fresh Tuna Range</h1>
                         <p className="subtitle my-3">Your choice of Hand Roll, Sashimi or Nigiri</p>
                         <a href="https://sushisushi.orderup.com.au/" className="btn btn-light">Start my order</a>
