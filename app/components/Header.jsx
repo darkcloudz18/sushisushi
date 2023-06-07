fb93e4a86826154ba781f255a670b5175e125720
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-06-07 11:18:02 +1000

    Header update (minus button icons)

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -29,7 +29,7 @@ export default function Header() {
                 </button>
                 </div>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav main-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <a className="nav-link" href="/our_menu">Menu</a>
                         </li>
