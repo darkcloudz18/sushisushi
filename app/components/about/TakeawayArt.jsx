4b8eaf4f28465eadc30ef5ba6967c36267485c2d
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-07-11 15:41:11 +1000

    New packaging photo

--- app/components/about/TakeawayArt.jsx
+++ app/components/about/TakeawayArt.jsx
@@ -11,7 +11,7 @@ export default function TakeawayArt() {
                     <p>We only wrap our sushi in the best. And we don’t just mean the highly sought after first batch Japanese nori. All of our packaging is sustainably produced as well. So it doesn’t just look beautiful, it’s recyclable to lessen any impact on the planet. Each piece in the series has been specifically designed as a feast for the eyes to be enjoyed while eating your beautiful sushi.</p>
                 </div>
                 <div className={"col-12 col-lg-6"}>
-                    <Image width={660} height={280} className={"img-fluid"} src={"/images/takeaway.jpeg"} alt={"Image showing a composition of the different art used by Sushi Sushi"}/>
+                    <Image width={1500} height={1001} className={"img-fluid"} src={"/images/box-stack.png"} alt={"Image showing a composition of the different art used by Sushi Sushi"}/>
                 </div>
             </div>
         </div>
