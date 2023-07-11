05d32c3979c49ced1fdb29f2e8ec43bef3910ee1
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-07-12 09:50:11 +1000

    left in fillerama

--- app/components/ts_and_cs/Competitions.jsx
+++ app/components/ts_and_cs/Competitions.jsx
@@ -4,16 +4,7 @@ export default function Competitions() {
     return(
         <div id="comps">
             <h3 className={"fw-bold"}>Competitions</h3>
-            <p>It&apos;s okay, Bender. I like cooking too. I&apos;ve been there. My folks were always on me to groom myself and wear underpants. What am I, the pope? Uh, is the puppy mechanical in any way? No. We&apos;re on the top.</p>
-            <p>Wow! A superpowers drug you can just rub onto your skin? You&apos;d think it would be something you&apos;d have to freebase. <strong> Good news, everyone!</strong> <em> I&apos;ve taught the toaster to feel love!</em> Can we have Bender Burgers again?</p>
-            <h4>I wish! It&apos;s a nickel.</h4>
-            <p>Who are you, my warranty?! I suppose I could part with &apos;one&apos; and still be feared… But I know you in the future. I cleaned your poop. You know the worst thing about being a slave? They make you work, but they don&apos;t pay you or let you go.</p>
-            <ol>
-                <li>Bender, quit destroying the universe!</li><li>Guards! Bring me the forms I need to fill out to have her taken away!</li><li>I haven&apos;t felt much of anything since my guinea pig died.</li>
-            </ol>
-
-            <h3>Also Zoidberg.</h3>
-            <p>Soon enough. What&apos;s with you kids? Every other day it&apos;s food, food, food. Alright, I&apos;ll get you some stupid food. You lived before you met me?! Isn&apos;t it true that you have been paid for your testimony?</p>
+            
         </div>
     );
 }
