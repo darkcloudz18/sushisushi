b608b0d4c56d06ed27cb5d0da393c139cad857ee
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-07-11 15:27:13 +1000

    Update Hawksburn.jsx

--- app/components/about/Hawksburn.jsx
+++ app/components/about/Hawksburn.jsx
@@ -7,7 +7,9 @@ export default function Hawksburn() {
         <div className="container py-5">
             <div className={"row d-flex align-items-center justify-content-center"}>
                 <div className={"col-12 col-lg-6 py-2"}>
-                    <p className={"lead fw-bold"}>Sushi Sushi x Bromley&Co concept store at Sushi Sushi Hawksburn. Sushi, hot food, art and unique Bromley x Sushi Sushi merchandise all available for purchase. Open 7 days.</p>
+                    <h2>Sushi Sushi x Bromley&Co</h2>
+                    <p>Our Sushi Sushi x Bromley&Co concept store is located in Hawksburn. Sushi, hot food, art and unique Bromley x Sushi Sushi merchandise all available for purchase.</p>
+                    <p>527 Malvern Road, Toorak Vic. Dine in, takeaway and delivery. Open 7 days.</p>
                 </div>
                 <div className={"col-12 col-lg-6 d-flex align-items-center justify-content-center"}>
                     <Image width={1024} height={591} className={"img-fluid"} src={"/images/hawksburn.jpeg"} alt={"Image the Hawksburn concept store"}/>
