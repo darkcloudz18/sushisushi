4b654429ba120f65c4bec26fd0e8020ab3455f9b
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-07-19 13:20:15 +1000

    blurb addition

--- app/family-and-friends-discount/page.js
+++ app/family-and-friends-discount/page.js
@@ -9,6 +9,9 @@ export default function FriendsAndFamily(){
 
         <>
             <Title title="Family and Friends Discount" headerClass={"stores"} />
+            <div className={"container"}>
+                <p className={"lead text-center mt-5"}>Hey, what kinda party is this? There's no booze and only one hooker. I had more, but you go ahead. Bender! Ship! Stop bickering or I'm going to come back there and change your opinions manually! I'm just glad my fat, ugly mama isn't alive to see this day.</p>
+            </div>
             <Map storeData={participatingStores} searchLabel={"Find your nearest participating store"}/>
         </>
 
