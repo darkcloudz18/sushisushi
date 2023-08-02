aeae185d4e1d068f7881da58af6c403d0f36116e
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-08-02 17:08:57 +1000

    salads and inari hero

--- app/components/homepage/Hero.jsx
+++ app/components/homepage/Hero.jsx
@@ -5,10 +5,11 @@ export default function Hero() {
         <div className="hero-container">
             <div className="container">
                 <div className="row">
-                    <div className="col-lg-6 my-5 my-lg-5 py-lg-2">
-                        <h1 className="hero-title">Fresh Tuna Range</h1>
-                        <p className="subtitle my-3">Your choice of Hand Roll, Sashimi or Nigiri</p>
-                        <a href="https://sushisushi.orderup.com.au/" className="btn btn-light">Start my order</a>
+                    <div className="col-lg-6 my-5 pb-lg-2">
+                        <img className={"img-fluid d-block d-lg-none"} alt={"All new salads and inari range"} src={"/images/hero/heading-mobile.png"}/>
+                        <img className={"img-fluid d-none d-lg-block"} alt={"All new salads and inari range"} src={"/images/hero/heading-desktop.png"}/>
+                        <p className="subtitle my-3">Send your taste buds on a trip with our new Japanese salads</p>
+                        <a href="/our-stores" className="btn btn-light">Find your nearest store</a>
                     </div>
                 </div>
             </div>
