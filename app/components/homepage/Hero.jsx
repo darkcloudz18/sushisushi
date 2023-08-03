07c3d6535afc7f09c75babd3563de6fffb3ee08b
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-08-03 15:28:58 +1000

    static images

--- app/components/homepage/Hero.jsx
+++ app/components/homepage/Hero.jsx
@@ -2,15 +2,8 @@
 
 export default function Hero() {
     return (
-        <div className="hero-container">
-                <div className="row">
-                    <div className="col-md-6 my-5 pb-md-2 px-5 px-md-0">
-                        <img className={"img-fluid d-block d-md-none"} alt={"All new salads and inari range"} src={"/images/hero/heading-mobile.png"}/>
-                        <img className={"img-fluid d-none d-md-block"} alt={"All new salads and inari range"} src={"/images/hero/heading-desktop.png"}/>
-                        <p className="subtitle my-3">Send your taste buds on a trip with our new Japanese&nbsp;salads</p>
-                        <a href="/our-stores" className="btn btn-sushi-primary">Find your nearest store</a>
-                    </div>
-            </div>
-        </div>
+        <a href="/our-stores"><div className="hero-container">
+
+        </div></a>
     );
 }
