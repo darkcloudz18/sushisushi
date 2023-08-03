2bf30718be828f6b00ed1a5e0529f2351eaa34f4
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-08-04 09:17:10 +1000

    hero w images as a separate thing

--- app/components/homepage/Hero.jsx
+++ app/components/homepage/Hero.jsx
@@ -2,8 +2,11 @@
 
 export default function Hero() {
     return (
-        <a href="/our-stores"><div className="hero-container">
-
-        </div></a>
+        <a href="/our-stores">
+            <div className="hero-container-img">
+                <img src={"/images/hero/desktop-full.png"} className={"img-fluid desktop-hero"}/>
+                <img src={"/images/hero/mobile-full.png"} className={"img-fluid mobile-hero"}/>
+            </div>
+        </a>
     );
 }
