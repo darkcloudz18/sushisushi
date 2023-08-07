e5139d58b02ac39e4e36f2523ca93ee83d987a5b
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-08-07 11:17:12 +1000

    koi of the month vid added

--- app/koi-of-the-month/page.js
+++ app/koi-of-the-month/page.js
@@ -3,9 +3,14 @@ import Title from "../components/Title";
 export default function KoiOfTheMonth() {
     return (
         <>
-            <Title title="Family and Friends Discount" headerClass={"stores"}></Title>
-            <div className={"container"}>
-                <video src={"/images/Sushisushi_Koiofthemonth_animation.mp4"}></video>
+            <Title title="Koi of the Month" headerClass={"stores"}></Title>
+            <div className={"container py-5"}>
+                <div className={"row"}>
+                    <div className={"col"}>
+                        <video width={1920} height={1080} src={"/images/Sushisushi_Koiofthemonth_animation.mp4"} className={"img-fluid mt-5"}
+                               autoPlay muted controls></video>
+                    </div>
+                </div>
             </div>
         </>
     );
