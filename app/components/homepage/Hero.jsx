645b62fa3df50dcd3c5d72804a117d151d1b68eb
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-08-14 10:06:54 +1000

    Hero carousel with prev/next buttons

--- app/components/homepage/Hero.jsx
+++ app/components/homepage/Hero.jsx
@@ -2,11 +2,35 @@
 
 export default function Hero() {
     return (
-        <a href="/our-stores">
-            <div className="hero-container-img">
-                <img src={"/images/hero/desktop-full.png"} className={"img-fluid desktop-hero"}/>
-                <img src={"/images/hero/mobile-full.png"} className={"img-fluid mobile-hero"}/>
+        <div id="heroCarousel" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <a href="/our-stores">
+                        <div className="hero-container-img">
+                            <img src={"/images/hero/asahi-desktop-full.jpg"} className={"img-fluid desktop-hero"}/>
+                            <img src={"/images/hero/asahi-mobile-full.jpg"} className={"img-fluid mobile-hero"}/>
+                        </div>
+                    </a>
+                </div>
+                <div className="carousel-item">
+                    <a href="/our-stores">
+                        <div className="hero-container-img">
+                            <img src={"/images/hero/desktop-full.png"} className={"img-fluid desktop-hero"}/>
+                            <img src={"/images/hero/mobile-full.png"} className={"img-fluid mobile-hero"}/>
+                        </div>
+                    </a>
+                </div>
             </div>
-        </a>
+            <button className="carousel-control-prev" type="button" data-bs-target="#heroCarousel"
+                    data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#heroCarousel"
+                    data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
     );
 }
