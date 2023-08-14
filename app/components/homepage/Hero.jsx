43493a153a6662da27939c28e772c1d59a41ab23
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-08-14 10:08:44 +1000

    Revert "Hero w/o prev/next buttons"
    This reverts commit c5d1cb4f6c830a4c46f0613adfe3753b12dae711.

--- app/components/homepage/Hero.jsx
+++ app/components/homepage/Hero.jsx
@@ -21,6 +21,16 @@ export default function Hero() {
                     </a>
                 </div>
             </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#heroCarousel"
+                    data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#heroCarousel"
+                    data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
         </div>
     );
 }
