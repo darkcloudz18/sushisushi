3db8d7f6763ee47bf837fa5de71a430c54c95702
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-08-14 10:10:39 +1000

    Update Hero.jsx

--- app/components/homepage/Hero.jsx
+++ app/components/homepage/Hero.jsx
@@ -2,25 +2,11 @@
 
 export default function Hero() {
     return (
-        <div id="heroCarousel" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <a href="/our-stores">
-                        <div className="hero-container-img">
-                            <img src={"/images/hero/asahi-desktop-full.jpg"} className={"img-fluid desktop-hero"}/>
-                            <img src={"/images/hero/asahi-mobile-full.jpg"} className={"img-fluid mobile-hero"}/>
-                        </div>
-                    </a>
-                </div>
-                <div className="carousel-item">
-                    <a href="/our-stores">
-                        <div className="hero-container-img">
-                            <img src={"/images/hero/desktop-full.png"} className={"img-fluid desktop-hero"}/>
-                            <img src={"/images/hero/mobile-full.png"} className={"img-fluid mobile-hero"}/>
-                        </div>
-                    </a>
-                </div>
+        <a href="/our-stores">
+            <div className="hero-container-img">
+                <img src={"/images/hero/desktop-full.png"} className={"img-fluid desktop-hero"}/>
+                <img src={"/images/hero/mobile-full.png"} className={"img-fluid mobile-hero"}/>
             </div>
-        </div>
+        </a>
     );
 }
