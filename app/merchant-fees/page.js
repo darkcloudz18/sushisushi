7a46155c0bb7ac873b91f32da2e6b4108f0a8ee7
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-08-22 11:01:43 +1000

    Merchant fees qr code page

---
+++ app/merchant-fees/page.js
@@ -0,0 +1,22 @@
+import Title from "../components/Title";
+
+export default function MerchantFees() {
+    return(
+        <>
+            <Title title="Merchant Fees" headerClass={"stores"}></Title>
+            <div className={"container py-5"}>
+                <p className={"lead"}>Amazing quality sushi is our number one priority, and we pride ourselves on only using the very best and freshest ingredients from local farmers and suppliers first, before sourcing from reputable suppliers overseas. Unfortunately, everywhere around the world has been impacted by the events of the past 2 years, and during this time we’ve had to wear the costs of labour shortages, and huge increases in ingredient and freight costs, all of which have put significant pressure on our business and continue to increase the cost of operations.</p>
+                <p>We always endeavour to absorb increases in costs rather than passing them onto our customers, however, we’ve reached a point where we’ve had to make some changes at most of our stores to ensure we can continue to bring you the freshest sushi every day, as follows:</p>
+                <h3>Merchant Fees</h3>
+                <p>We have started charging a small transaction fee for all card transactions, based on the merchant fees charged by the banking institutions for that store.</p>
+                <p>As this amount can vary from location to location, this transaction fee is displayed as a percentage on the sign you have scanned to be directed to this page.</p>
+                <p>There are other ways to pay for your transactions at our stores without incurring a surcharge, such as by paying by cash. However, this surcharge applies to all products paid using our online services and is unavoidable for online purchases.</p>
+                <h3>Public Holiday and Sunday Surcharge</h3>
+                <p>We’ve had to introduce a 15% surcharge on Public Holidays and a 10% surcharge on Sundays to ensure we can continue to bring you the freshest sushi and a great experience at every opportunity all year-round.</p>
+                <p>We value our customers and are extremely appreciative of the support that our business has received from the community for the past 20 years. If you have any questions or would like to share you feedback with us, we’d love to hear from you. Please email us directly at customerfeedback@sushisushi.com.au</p>
+                <p>We look forward to continuing to bring you the freshest, most delicious sushi every day.</p>
+                <p>The team at Sushi Sushi</p>
+            </div>
+        </>
+    );
+}
