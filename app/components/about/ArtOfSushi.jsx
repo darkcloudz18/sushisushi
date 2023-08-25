0029b56e8bf0a2f6f55e34e4bbac7be9f18dabee
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-08-25 14:20:44 +1000

    Unnecessary "opened"

--- app/components/about/ArtOfSushi.jsx
+++ app/components/about/ArtOfSushi.jsx
@@ -8,7 +8,7 @@ export default function ArtOfSushi() {
         <div className="container py-5">
             <div className={"row d-flex align-items-center justify-content-center"}>
                 <div className={"col-12 col-lg-6 py-2"}>
-                    <p className={"lead fw-bold"}>Back in 1998 we opened our first store opened in Box Hill, Victoria with aspirations to change how Australians eat sushi. Our dream was to take it from high-end Japanese restaurants and make it a fresh, healthy on the go meal available for everyone to enjoy.</p>
+                    <p className={"lead fw-bold"}>Back in 1998 we opened our first store in Box Hill, Victoria with aspirations to change how Australians eat sushi. Our dream was to take it from high-end Japanese restaurants and make it a fresh, healthy on the go meal available for everyone to enjoy.</p>
                     <p>So successful was the venture, we’re now approaching 170 Sushi Sushi stores nationally, making us the biggest and most loved sushi brand in Australia.</p>
                     <p>We’ve put the impossible to find everywhere and made the complex simple. We’re the sushi pioneers of Australia and that’s a lot to be proud of!</p>
                     <p>Our success comes from the quality and consistency of our sushi. We’ve made little change to our core product and format over the last 25 years, because after all it’s stood the test of time for thousands of years in Japan.</p>
