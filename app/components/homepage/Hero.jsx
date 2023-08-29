3fd88741d3a552d782fc164c2e8c32c05ec15b41
pascale-sushi <126639919+pascale-sushi@users.noreply.github.com>
2023-08-29 13:55:43 +1000

    Bento and Poke hero added

--- app/components/homepage/Hero.jsx
+++ app/components/homepage/Hero.jsx
@@ -7,6 +7,14 @@ export default function Hero() {
                 <div className="carousel-item active">
                     <a href="/our-stores">
                         <div className="hero-container-img">
+                            <img src={"/images/hero/bento-desktop.jpg"} className={"img-fluid desktop-hero"}/>
+                            <img src={"/images/hero/bento-mobile.jpg"} className={"img-fluid mobile-hero"}/>
+                        </div>
+                    </a>
+                </div>
+                <div className="carousel-item">
+                    <a href="/our-stores">
+                        <div className="hero-container-img">
                             <img src={"/images/hero/asahi-desktop-full.png"} className={"img-fluid desktop-hero"}/>
                             <img src={"/images/hero/asahi-mobile-full.png"} className={"img-fluid mobile-hero"}/>
                         </div>
